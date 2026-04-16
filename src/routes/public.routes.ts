8d5e29b9aaa0948a104f96f243ec88056cc158eb
ktyudha <ktyudha@gmail.com>
2026-04-16 13:10:34 +0700

    feat(public): add route send message & media url

--- src/routes/public.routes.ts
+++ src/routes/public.routes.ts
@@ -1,11 +1,27 @@
 import { Hono } from "hono";
 import { publicController } from "../controllers/public.controller";
+import { whatsappController } from "../controllers/whatsapp.controller";
 
 const publicRoutes = new Hono();
 
 // Send Message Group / Chat
 publicRoutes.post("/send-message-global", (c) =>
     publicController.sendMessageGlobal(c)
+);
+
+// Send Message Chat
+publicRoutes.post("/send-message", (c) =>
+    whatsappController.sendMessage(c)
+);
+
+// Send Message Group
+publicRoutes.post("/send-message-group", (c) =>
+    whatsappController.sendMessage(c)
+);
+
+// Send Media Url Group / Chat
+publicRoutes.post("/send-media-url", (c) =>
+    whatsappController.sendMediaWithUrl(c)
 );
 
 export default publicRoutes;
