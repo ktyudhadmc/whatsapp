d5c71cfa20c9e2d56f8a4572c665a46d811ad2e4
ktyudha <ktyudha@gmail.com>
2026-04-16 14:52:30 +0700

    fix(public): moving controller route

--- src/routes/public.routes.ts
+++ src/routes/public.routes.ts
@@ -6,22 +6,20 @@ const publicRoutes = new Hono();
 
 // Send Message Group / Chat
 publicRoutes.post("/send-message-global", (c) =>
-    publicController.sendMessageGlobal(c)
+  publicController.sendMessageGlobal(c),
 );
 
 // Send Message Chat
-publicRoutes.post("/send-message", (c) =>
-    whatsappController.sendMessage(c)
-);
+publicRoutes.post("/send-message", (c) => whatsappController.sendMessage(c));
 
 // Send Message Group
 publicRoutes.post("/send-message-group", (c) =>
-    whatsappController.sendMessage(c)
+  whatsappController.sendMessageToGroup(c),
 );
 
 // Send Media Url Group / Chat
 publicRoutes.post("/send-media-url", (c) =>
-    whatsappController.sendMediaWithUrl(c)
+  whatsappController.sendMediaWithUrl(c),
 );
 
 export default publicRoutes;
